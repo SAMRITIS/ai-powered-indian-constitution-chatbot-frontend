c14badfbfdd0cc1d689fc01c104aa1b43dd82f99
SAMRITIS <saurabhsinghggu@gmail.com>
2025-11-08 12:54:12 +0530

    vit-config-updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ai-powered-indian-constitution-chatbot-frontend/', // 👈 add this line
 })
